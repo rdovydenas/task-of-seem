59292237fa9038e4b8e986c92c50504cab38f324
Rolandas <rdovydenas@protonmail.ch>
2022-04-07 19:17:10 +0300

    done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
           />
           <img src={user} alt="" />
         </label>
-
+       
         <label htmlFor="password">
           <input
             type="password"
